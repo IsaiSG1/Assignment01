4d1233b5726cdeb0b4c0db122bdef090c69c076d
isai silva <isaisilva11@gmail.com>
2024-02-21 18:15:59 -0800

    Complete

--- js/script.js
+++ js/script.js
@@ -5,5 +5,119 @@ let currentMonth
 let SummerMonth
 let myLibraryFunction
 //STEP 2
+result = 6+7
+write = 'Hello everybody'
+result = 7>5 
+result = none;
 
 //STEP 3
+var x = 28
+let birthdayMonth = 'November'
+var message = " I will be turning " + x + " in " + birthdayMonth + "."
+console.log(message)
+
+let length = 10;
+let width = 5;
+let area = length * width;
+console.log("The area of the rectangle is: " + area);
+//STEP4
+let strFirstName
+let strLastName
+let strAddress
+let strCity
+let strState
+let strZipCode
+let intYourAge
+let strReferralSource
+let boolMayWeContactYou
+
+//STEP 5
+strFirstName = "Isai"
+strLastName = "Silva"
+strCity = "San Diego"
+//STEP 6
+let num = 19;
+console.log(num + "5")
+
+//STEP7
+let boolStrVar = true + " hello"
+console.log(boolStrVar)
+let numBoolVar = 10 + true
+console.log(numBoolVar)
+
+//STEP8
+//yes the sting literal is valid.
+//STEP9
+
+let nullValue = null
+console.log(nullValue)
+
+let undefinedValue
+console.log(undefinedValue)
+
+//STEP10
+console.log(typeof "Hello")
+console.log(typeof 42)
+console.log(typeof true)
+console.log(typeof {})
+console.log(typeof undefined)
+
+//STEP11
+
+alert("Hello Isai Silva, welcome to the JavaScript class!")
+
+//STEP12
+
+let name = "Isai Silva"
+
+//STEP13
+
+let course = "JavaScript"
+
+//STEP14
+
+alert("Hello " + name + ".\nWelcome to the " + course + " class!")
+
+//STEP15
+
+let name = prompt("What is your name?")
+
+//STEP16
+
+let course = prompt("What class are you taking?")
+
+//STEP17
+
+let x = 10
+let y = 20
+console.log(x + y)
+
+//STEP18
+
+let x = 20
+x += 20
+console.log(x)
+
+//STEP19
+
+let x = 20
+x *= 5
+console.log(x)
+
+//STEP20
+
+let x = 20 % 3
+x /= 1
+console.log(x)
+
+//STEP21	
+
+let a = 10
+let b = 5
+console.log(a > b && b > 2)
+
+//STEP22
+
+let c = 10;
+let d = 5;
+console.log(c < d || c === 10)
